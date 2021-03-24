93c89b63178da1a2f63df55dfbcf46ea02b6ea0c
alexis <alexritti111@gmail.com>
2021-03-24 11:50:15 +0100

    first commit

--- js/app.js
+++ js/app.js
@@ -15,7 +15,7 @@ document.addEventListener('click', ()=>{
 
 })
  const load=document.querySelector('.loader');
- 
+
   window.addEventListener('load',function() {
 
 	var lineBar = new ProgressBar.Line("#line-container", {
@@ -35,5 +35,58 @@ document.addEventListener('click', ()=>{
 	  lineBar.animate(1, {
 		duration: 1000,
 	  }, function(){ load.style.opacity="0";});
-	
+
+});
+
+// MENU BURGER
+
+$(document).ready(function(){
+  $(window).mousemove(function(e){
+    var y = e.clientY;
+    // console.log(y);
+    $(".hover-element").css("margin-top", y);
+  });
+
+  $("nav ul li a").mouseover(function(){
+    var activeLinkId = $(this).attr("href");
+    $(".link-img img").removeClass("active");
+    $(".link-img img"+activeLinkId).addClass("active");
+  });
+
+  var toggleBtn = document.querySelector(".toggle");
+  //GSAP timeline
+  var tl = new TimelineMax({paused: true});
+
+  tl.to(".toggle span:nth-child(2)", 1, {
+    right: "-100"
+  })
+  tl.to(".toggle span:nth-child(1)", 1, {
+    rotate: 45,
+    top: 10
+  }, "-=1")//offset
+  tl.to(".toggle span:nth-child(3)", 1, {
+    rotate: -45,
+    top: 10
+  }, "-=1")//offset
+
+  tl.to("nav", 1, {
+    left: "0"
+  }, "-=1")//Offset
+
+  tl.from("nav ul li", 1, {
+    x: -300,
+    opacity: 0,
+    stagger: .2
+  })
+  tl.from(".link-img", 1, {
+    scale: 1.1,
+    opacity: 0
+  })
+
+  // Timeine reverse
+  tl.reverse();
+  //Click to play Timeline
+  toggleBtn.onclick = function(){
+    tl.reversed(!tl.reversed());
+  }
 });
